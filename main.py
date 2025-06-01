5cfa23e308fee0df6e10f0903ff0fd05bf619e13
mauricep0205 <mauricep0205@gmail.com>
2025-06-01 19:40:27 -0400

    Added player to game

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     running = True
@@ -11,7 +15,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit fps to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
