6a907f8b493a4d474d38dcbfc76f81af713c803f
mauricep0205 <mauricep0205@gmail.com>
2025-06-01 20:59:04 -0400

    Asteroids can be shot

--- main.py
+++ main.py
@@ -37,6 +37,10 @@ def main():
         updatable.update(dt)
 
         for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.kill()
+                    shot.kill()
             if asteroid.collides_with(player):
                 print("Game over!")
                 sys.exit()
